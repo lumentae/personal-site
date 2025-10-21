889b67fd01c994f36981363d1658424e489c0465
lumentae <66220444+lumentae@users.noreply.github.com>
2025-10-21 15:14:46 +0200

    Add transition to card

--- components/container/Card.tsx
+++ components/container/Card.tsx
@@ -7,7 +7,7 @@ type CardProps = {
 
 export default function Card({ children, style }: CardProps) {
     return (
-        <div className="rounded-lg border border-white/20 hover:border-white/50 transition-colors backdrop-blur-md hover:bg-stone-800/30 shadow-2xl p-3 h-full" style={style}>
+        <div className="rounded-lg border border-white/20 hover:border-white/50 transition duration-300 backdrop-blur-md hover:bg-stone-800/30 shadow-2xl p-3 h-full" style={style}>
             {children}
         </div>
     );
