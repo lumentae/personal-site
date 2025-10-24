8d7e50393234cdd13a0d6ac131541f8b0f7ba6b0
lumentae <66220444+lumentae@users.noreply.github.com>
2025-10-24 12:01:30 +0200

    Add footer

--- components/container/Content.tsx
+++ components/container/Content.tsx
@@ -6,8 +6,16 @@ type ContentProps = {
 
 export default function Content({children}: ContentProps) {
     return (
-        <div className="space-y-4 pr-2">
+        <div className="space-y-4">
             {children}
+            <footer className="flex justify-center text-center">
+                <p className="darken text-xs">
+                    Made with NextJS.
+                    Source available on <a className="darken underline" href="https://github.com/lumentae/personal-site" target="_blank">GitHub</a>.
+                    Licensed under MIT.
+                    © Copyright 2025 lumentae.
+                </p>
+            </footer>
         </div>
     );
 }
