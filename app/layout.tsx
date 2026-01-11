9e85bb9cdd9f3d7ce4457fa52761613eec4fc727
lumentae <me@lumentae.dev>
2026-01-12 00:50:56 +0100

    Change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,33 +5,33 @@ import Background from "@/components/background/background";
 import BodyStyleModifiers from "@/components/BodyStyleModifiers";
 
 export const metadata: Metadata = {
-  title: "lumentae's personal site",
-  description: "this is lumentae's awesome personal site!",
+    title: "lumentae's personal site",
+    description: "this is lumentae's awesome personal site!",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <head>
-        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
-        <link rel="icon" href="favicon.png" sizes="any" />
-        <script src="https://a.lumentae.dev/script.js" data-website-id={`${process.env.WEBSITE_ID}`} defer></script>
-      </head>
-      <body
-        className="antialiased"
-        style={{ colorScheme: "dark" }}
-      >
-        <div className="h-screen w-screen">
-          <Background />
-        </div>
-        <Oneko />
-        <BodyStyleModifiers />
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
+                <link rel="icon" href="favicon.jpg" sizes="any" />
+                <script src="https://a.lumentae.dev/script.js" data-website-id={`${process.env.WEBSITE_ID}`} defer></script>
+            </head>
+            <body
+                className="antialiased"
+                style={{ colorScheme: "dark" }}
+            >
+                <div className="h-screen w-screen">
+                    <Background />
+                </div>
+                <Oneko />
+                <BodyStyleModifiers />
+                {children}
+            </body>
+        </html>
+    );
 }
