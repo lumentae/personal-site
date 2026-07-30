e441124f80a240d8347b97f7fb9659bdf7d1df4f
lumentae <me@lumentae.dev>
2026-07-31 01:20:39 +0200

    Add minimal astro starter kit

--- components/HorizontalDivider.tsx
+++
@@ -1,3 +0,0 @@
-export default function HorizontalDivider() {
-  return <hr className="text-white/40 mb-6" />;
-}
--- components/VerticalDivider.tsx
+++
@@ -1,3 +0,0 @@
-export default function VerticalDivider() {
-  return <p className="text-white/40 mb-6">|</p>;
-}
